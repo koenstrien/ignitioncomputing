b52e4bc7867cef35ae1bc772dc22f99091444fc3
Yannick de Jong <ydejong@ignitioncomputing.com>
2024-04-03 15:30:21 +0200

    Cleanup canvas CTX

--- assets/js/confetti.js
+++ assets/js/confetti.js
@@ -1,73 +1,62 @@
-class Confetti {
-    static CTX = null;
-    static CONFIG = {
-        gravity: 10,
-        particle_count: 75,
-        particle_size: 1,
-        explosion_power: 25,
-        destroy_target: true,
-        fade: false
-    };
-
+class ConfettiContext {
     constructor() {
+        this.gravity = 10
+        this.particle_count = 75
+        this.particle_size = 1
+        this.explosion_power = 25
+        this.fade = false
+        this.setupCanvasContext()
+    }
 
+    setupCanvasContext() {
+        this.canvas = document.createElement("canvas");
+        this.canvas_ctx = this.canvas.getContext("2d");
+        this.canvas.width = 2 * window.innerWidth;
+        this.canvas.height = 2 * window.innerHeight;
+        Object.assign(this.canvas.style, {
+            position: "fixed",
+            top: "0",
+            left: "0",
+            width: "calc(100%)",
+            height: "calc(100%)",
+            margin: "0",
+            padding: "0",
+            zIndex: "999999999",
+            pointerEvents: "none"
+        });
+        document.body.appendChild(this.canvas);
+        window.addEventListener("resize", () => {
+            this.canvas.width = 2 * window.innerWidth;
+            this.canvas.height = 2 * window.innerHeight;
+        });
+    }
+
+    clearScreen() {
+        this.canvas_ctx.clearRect(0, 0, 2 * window.innerWidth, 2 * window.innerHeight);
+    }
+
+    drawRectangle(position, size, rotation, hue, opacity) {
+        this.canvas_ctx.save();
+        this.canvas_ctx.beginPath();
+        this.canvas_ctx.translate(position.x + size.x / 2, position.y + size.y / 2);
+        this.canvas_ctx.rotate(rotation * Math.PI / 180);
+        this.canvas_ctx.rect(-size.x / 2, -size.y / 2, size.x, size.y);
+        this.canvas_ctx.fillStyle = `hsla(${hue}deg, 90%, 65%, ${opacity}%)`;
+        this.canvas_ctx.fill();
+        this.canvas_ctx.restore();
+    }
+}
+
+class Confetti {
+    constructor() {
         this.bursts = [];
         this.time = new Date().getTime();
         this.delta_time = 0;
 
-        this.setupCanvasContext();
+        this.context = new ConfettiContext()
+
         this.set_listeners();
         window.requestAnimationFrame(this.update.bind(this));
-    }
-
-    setCount(count) {
-        if (typeof count !== 'number') throw new Error("Input must be of type 'number'");
-        Confetti.CONFIG.particle_count = count;
-    }
-
-    setPower(power) {
-        if (typeof power !== 'number') throw new Error("Input must be of type 'number'");
-        Confetti.CONFIG.explosion_power = power;
-    }
-
-    setSize(size) {
-        if (typeof size !== 'number') throw new Error("Input must be of type 'number'");
-        Confetti.CONFIG.particle_size = size;
-    }
-
-    setFade(fade) {
-        if (typeof fade !== 'boolean') throw new Error("Input must be of type 'boolean'");
-        Confetti.CONFIG.fade = fade;
-    }
-
-    destroyTarget(destroy) {
-        if (typeof destroy !== 'boolean') throw new Error("Input must be of type 'boolean'");
-        Confetti.CONFIG.destroy_target = destroy;
-    }
-
-    setupCanvasContext() {
-        if (!Confetti.CTX) {
-            const canvas = document.createElement("canvas");
-            Confetti.CTX = canvas.getContext("2d");
-            canvas.width = 2 * window.innerWidth;
-            canvas.height = 2 * window.innerHeight;
-            Object.assign(canvas.style, {
-                position: "fixed",
-                top: "0",
-                left: "0",
-                width: "calc(100%)",
-                height: "calc(100%)",
-                margin: "0",
-                padding: "0",
-                zIndex: "999999999",
-                pointerEvents: "none"
-            });
-            document.body.appendChild(canvas);
-            window.addEventListener("resize", () => {
-                canvas.width = 2 * window.innerWidth;
-                canvas.height = 2 * window.innerHeight;
-            });
-        }
     }
 
     set_listeners() {
@@ -75,7 +64,7 @@ class Confetti {
         for (let element of elements) {
             element.addEventListener("click", (event) => {
                 const burstPosition = new Point(2 * event.clientX, 2 * event.clientY);
-                this.bursts.push(new ConfettiBurst(burstPosition));
+                this.bursts.push(new ConfettiBurst(burstPosition, this.context));
             });
         }
     }
@@ -94,18 +83,17 @@ class Confetti {
     }
 
     draw() {
-        CanvasHelper.clearScreen();
+        this.context.clearScreen();
         this.bursts.forEach(burst => burst.draw());
     }
 }
 
 class ConfettiBurst {
-    static CTX = null;
-
-    constructor(position) {
+    constructor(position, context) {
+        this.context = context
         this.particles = [];
-        for (let i = 0; i < Confetti.CONFIG.particle_count; i++) {
-            this.particles.push(new TreatBurstParticle(position));
+        for (let i = 0; i < context.particle_count; i++) {
+            this.particles.push(new TreatBurstParticle(position, context));
         }
     }
 
@@ -135,17 +123,18 @@ function hslToHex(h, s, l) {
 }
 
 class TreatBurstParticle {
-    constructor(position) {
+    constructor(position, context) {
+        this.context = context
         this.scale = 16 * Math.random() + 4
-        this.size = new Point(this.scale * Confetti.CONFIG.particle_size, this.scale * Confetti.CONFIG.particle_size);
+        this.size = new Point(this.scale * context.particle_size, this.scale * context.particle_size);
         this.position = new Point(position.x - this.size.x / 2, position.y - this.size.y / 2);
-        this.velocity = generateVelocity();
+        this.velocity = generateVelocity(context);
         this.rotation = 360 * Math.random();
         this.rotation_speed = 10 * (Math.random() - 0.5);
         this.hue = 360 * Math.random();
         this.image = new Image();
         this.image.src = 'data:image/svg+xml,' + encodeURIComponent(`
-            <svg fill="${hslToHex(this.hue, 100, 50)}" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="800px" height="800px" viewBox="0 0 885.851 885.851" xml:space="preserve">
+            <svg fill="${hslToHex(this.hue, 90, 65)}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="800px" height="800px" viewBox="0 0 885.851 885.851" xml:space="preserve">
                 <path d="M755.125,130.775c1.399-33.9-10.8-68.2-36.7-94.1c-49.1-49.101-128.4-48.801-177.5,0.3c-38.8,38.8-47.5,96-24.2,143.6    l-336.1,336.2c-47.6-23.3-104.8-14.6-143.6,24.2c-49.1,49-49.3,128.5-0.2,177.6c25.9,25.9,60.1,38,94,36.601    c-1.4,33.899,10.8,68.2,36.7,94c49.1,49.1,128.4,48.8,177.5-0.3c38.801-38.801,47.4-96,24.101-143.601l336.1-336.1    c47.601,23.3,104.8,14.6,143.601-24.2c49.1-49.101,49.3-128.601,0.199-177.7C823.125,141.476,789.025,129.375,755.125,130.775z"/>
             </svg>
         `);
@@ -154,14 +143,14 @@ class TreatBurstParticle {
     }
 
     update(time) {
-        this.velocity.y += Confetti.CONFIG.gravity * (this.size.y / (10 * Confetti.CONFIG.particle_size)) * time;
+        this.velocity.y += this.context.gravity * (this.size.y / (10 * this.context.particle_size)) * time;
         this.velocity.x += 25 * (Math.random() - 0.5) * time;
         this.velocity.y *= 0.98;
         this.velocity.x *= 0.98;
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
         this.rotation += this.rotation_speed;
-        if (Confetti.CONFIG.fade) {
+        if (this.context.fade) {
             this.opacity -= this.lifetime;
         }
     }
@@ -172,25 +161,24 @@ class TreatBurstParticle {
 
     draw() {
         if (this.image.complete) {
-            if (Confetti.CTX) {
-                let context = Confetti.CTX
+                let context = this.context.canvas_ctx
                 let x = this.position.x
                 let y = this.position.y
                 context.save();
                 context.translate(x, y);
-                context.rotate(this.rotation*Math.PI/180);
+                context.rotate(this.rotation * Math.PI / 180);
                 context.drawImage(this.image, 0, 0, this.size.x, this.size.y);
                 context.restore()
-            }
         }
     }
 }
 
 class ConfettiBurstParticle {
-    constructor(position) {
-        this.size = new Point((16 * Math.random() + 4) * Confetti.CONFIG.particle_size, (4 * Math.random() + 4) * Confetti.CONFIG.particle_size);
+    constructor(position, context) {
+        this.context = context
+        this.size = new Point((16 * Math.random() + 4) * context.particle_size, (4 * Math.random() + 4) * context.particle_size);
         this.position = new Point(position.x - this.size.x / 2, position.y - this.size.y / 2);
-        this.velocity = generateVelocity();
+        this.velocity = generateVelocity(context);
         this.rotation = 360 * Math.random();
         this.rotation_speed = 10 * (Math.random() - 0.5);
         this.hue = 360 * Math.random();
@@ -199,14 +187,14 @@ class ConfettiBurstParticle {
     }
 
     update(time) {
-        this.velocity.y += Confetti.CONFIG.gravity * (this.size.y / (10 * Confetti.CONFIG.particle_size)) * time;
+        this.velocity.y += this.context.gravity * (this.size.y / (10 * this.context.particle_size)) * time;
         this.velocity.x += 25 * (Math.random() - 0.5) * time;
         this.velocity.y *= 0.98;
         this.velocity.x *= 0.98;
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
         this.rotation += this.rotation_speed;
-        if (Confetti.CONFIG.fade) {
+        if (this.context.fade) {
             this.opacity -= this.lifetime;
         }
     }
@@ -216,7 +204,7 @@ class ConfettiBurstParticle {
     }
 
     draw() {
-        CanvasHelper.drawRectangle(this.position, this.size, this.rotation, this.hue, this.opacity);
+        this.context.drawRectangle(this.position, this.size, this.rotation, this.hue, this.opacity);
     }
 }
 
@@ -227,35 +215,14 @@ class Point {
     }
 }
 
-function generateVelocity() {
+function generateVelocity(context) {
     const x = Math.random() - 0.5;
     const y = Math.random() - 0.7;
     const magnitude = Math.sqrt(x * x + y * y);
     return new Point(
-        x / magnitude * (Math.random() * Confetti.CONFIG.explosion_power),
-        y * (Math.random() * Confetti.CONFIG.explosion_power)
+        x / magnitude * (Math.random() * context.explosion_power),
+        y * (Math.random() * context.explosion_power)
     );
-}
-
-class CanvasHelper {
-    static clearScreen() {
-        if (Confetti.CTX) {
-            Confetti.CTX.clearRect(0, 0, 2 * window.innerWidth, 2 * window.innerHeight);
-        }
-    }
-
-    static drawRectangle(position, size, rotation, hue, opacity) {
-        if (Confetti.CTX) {
-            Confetti.CTX.save();
-            Confetti.CTX.beginPath();
-            Confetti.CTX.translate(position.x + size.x / 2, position.y + size.y / 2);
-            Confetti.CTX.rotate(rotation * Math.PI / 180);
-            Confetti.CTX.rect(-size.x / 2, -size.y / 2, size.x, size.y);
-            Confetti.CTX.fillStyle = `hsla(${hue}deg, 90%, 65%, ${opacity}%)`;
-            Confetti.CTX.fill();
-            Confetti.CTX.restore();
-        }
-    }
 }
 
 c = new Confetti()
